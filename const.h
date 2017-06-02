729474763af0f4de7279248b6283b04cad3d90db
yacropolisy <cancer_acropolis@yahoo.co.jp>
2017-06-02 23:18:05 +0900

    [change] this can simulate

--- const.h
+++ const.h
@@ -1,11 +1,11 @@
 #ifndef INCLUDED_const_h
 #define INCLUDED_const_h
 
-#define T 3
+#define T 600
 #define M 91
-#define Nc 3
-#define Nv 3
-#define Lambda 0.01
+#define Nc 500
+#define Nv 500
+#define Lambda 0.05
 #define Interval 30
 #define Datasize 0.1
 
