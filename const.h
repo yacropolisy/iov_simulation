7643d9e14fe20cf9c150de6aa8ca400a2b71e60a
yacropolisy <cancer_acropolis@yahoo.co.jp>
2017-06-07 18:47:59 +0900

    [change] adjust U0

--- const.h
+++ const.h
@@ -7,7 +7,7 @@
 #define Nv 3
 #define Lambda 0.01
 #define Interval 30
-#define Datasize 0.1
-#define U0 0.1
+#define Datasize 0.1  //user数:500,500 Lamda:0.01 Interval:30の時0.1くらいでトラヒックはサチらない
+#define U0 1  //1の時が程よく反応率が数１０％になる
 
 #endif
